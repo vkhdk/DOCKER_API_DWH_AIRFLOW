4f466eb43c36db3221761813f58e815f27cb548f
Vova Kh <VKHDK@yandex.ru>
2024-03-11 11:12:53 +0300

    working version

--- DWH/file_share/init/02_load_dump.sh
+++ DWH/file_share/init/02_load_dump.sh
@@ -1,2 +1,3 @@
 # load data into the database from dump
-psql -c 'SET search_path TO bd_shops;' -U dwh_postgres -d dwh_shops  -f /var/lib/postgresql/dumps/dump1.sql
+current_dump="dump1.sql"
+psql -c 'SET search_path TO bd_shops;' -U dwh_postgres -d dwh_shops  -f /var/lib/postgresql/dumps/$current_dump
